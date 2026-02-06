fef9ce386b28cd2d74f508665005370a3609ff3a
IT Sysdev Rco <it11.support@rcoid.com>
2026-02-06 09:54:34 +0800

    Update index.ts

--- apps/backend/src/controllers/salesPerson/index.ts
+++ apps/backend/src/controllers/salesPerson/index.ts
@@ -1,7 +1,6 @@
 import { ISalesPerson } from '@saleshub-tsm/types';
 import { Request, Response } from 'express';
 import prisma from '@/libs/prisma.js';
-import dayjs from 'dayjs';
 
 export type SalsePersonResponseType = {
   message: string;
@@ -12,23 +11,26 @@ export type SalsePersonResponseType = {
 export const salesPersons = async (req: Request, res: Response<SalsePersonResponseType>) => {
   try {
     const { withFilterUser } = req.query;
-    let where: any = {
-      Active: 'Y',
-      Locked: 'N',
-      SlpCode: { gt: 0 },
-    };
 
-    const withUser = String(withFilterUser) === '1' || String(withFilterUser) === 'true';
-
-    if (withUser) {
-      where.user = null
-    } else {
-      where.user = { isNot: null };
-    }
+    const withUser =
+      String(withFilterUser) === '1' || String(withFilterUser) === 'true';
 
     const salesPersons = await prisma.sales_persons.findMany({
-      where,
-      distinct: ['SlpCode']
+      where: {
+        Active: 'Y',
+        Locked: 'N',
+        SlpCode: { gt: 0 },
+
+        ...(withUser && {
+          user: null,
+        }),
+      },
+
+      include: {
+        user: true,
+      },
+
+      distinct: ['SlpCode'],
     });
 
     return res
