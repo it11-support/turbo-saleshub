ed73eca0f2d2f89e3bb0e5bb89d40809b9caac6e
IT Sysdev Rco <it11.support@rcoid.com>
2026-06-05 11:39:13 +0800

    Update jwt.ts

--- apps/backend/src/utils/jwt.ts
+++ apps/backend/src/utils/jwt.ts
@@ -4,7 +4,11 @@ import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
-const JWT_SECRET = process.env.JWT_SECRET || 'supersecret';
+if (!process.env.JWT_SECRET) {
+  throw new Error('FATAL ERROR: JWT_SECRET is not defined');
+}
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export const generateToken = (payload: object, expiry?: any) => {
   return jwt.sign(payload, JWT_SECRET, { expiresIn: expiry || '1d' });
