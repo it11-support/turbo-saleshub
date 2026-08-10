6ecbf332bfdfba0afbb008180b331b8425f77657
IT Sysdev Rco <it11.support@rcoid.com>
2026-08-10 10:22:17 +0800

    Update prisma.ts

--- apps/backend/src/libs/prisma.ts
+++ apps/backend/src/libs/prisma.ts
@@ -120,7 +120,7 @@ const adapter = new PrismaMariaDb({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  port: Number(process.env.DATABSE_PORT || 3306),
+  port: Number(process.env.DATABASE_PORT || 3306),
   connectionLimit: 20
 });
 
